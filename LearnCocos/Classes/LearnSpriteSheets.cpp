e3cecc276d59fe638d966ea35be82703c811c9e2
maxsdrv <maxim_sdrv@mail.ru>
2022-06-23 16:38:30 +0300

    'added new class with animation'

--- LearnCocos/Classes/LearnSpriteSheets.cpp
+++ LearnCocos/Classes/LearnSpriteSheets.cpp
@@ -12,7 +12,15 @@ cocos2d::Scene *LearnSpriteSheets::createScene()
 }
 cocos2d::Vector<cocos2d::SpriteFrame *> LearnSpriteSheets::getAnimation(const char *format, int count)
 {
-    return cocos2d::Vector<cocos2d::SpriteFrame *>();
+    auto sprite_cache = SpriteFrameCache::getInstance();
+    Vector<SpriteFrame*> anim_frames;
+    char str[100];
+    for (int i = 0; i <= count; ++i)
+    {
+        sprintf(str, format, i);
+        anim_frames.pushBack(sprite_cache->getSpriteFrameByName(str));
+    }
+    return anim_frames;
 }
 bool LearnSpriteSheets::init()
 {
@@ -30,6 +38,21 @@ bool LearnSpriteSheets::init()
     auto background = Sprite::createWithSpriteFrameName("Cartoon_Forest_BG_02.png");
     background->setPosition(origin.x + visible_size.x / 2, origin.y + visible_size.y / 2);
     this->addChild(background);
+
+    //sprite
+    auto frames = getAnimation("sprite-sheet-test/tile%03d.png", 3);
+    auto sprite = Sprite::createWithSpriteFrame(frames.front());
+    background->addChild(sprite);
+    sprite->setPosition(100, 120);
+
+    auto animation = Animation::createWithSpriteFrames(frames, 1.0f/4);
+    sprite->runAction(RepeatForever::create(Animate::create(animation)));
+
+    //move to
+    auto movement = MoveTo::create(10, Vec2(1500, 100));
+    auto resetPosition = MoveTo::create(0, Vec2(-150, 120));
+    auto sequence = Sequence::create(movement, resetPosition, nullptr);
+    sprite->runAction(RepeatForever::create(sequence));
 
     return true;
 }
